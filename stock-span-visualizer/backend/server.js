b55f4773e1881ebd28c0016fd2ad528758f9f770
pratik pankaj <pratikpankaj04@gmail.conm>
2025-06-22 12:52:32 +0530

    Updated PORT to use process.env for deployment compatibility

--- stock-span-visualizer/backend/server.js
+++ stock-span-visualizer/backend/server.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3002;
+const PORT =process.env.PORT || 3002;;
 
 app.use(cors());
 app.use(express.json());
